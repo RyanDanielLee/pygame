5e0d541cbe7091c80bf2543b65bc25283149432e
Ryan <rkoryan88@outlook.com>
2023-12-04 21:49:36 -0800

    Fixed camera angles

--- components/platform.py
+++ components/platform.py
@@ -8,16 +8,15 @@ class Platform(pygame.sprite.Sprite):
         self.image_path = image_path
         self.original_image = pygame.image.load(image_path).convert()  # Load the original image
 
-        print(f"Original image size: {self.original_image.get_size()}")  # Debug line
-
         # If the image has transparency, set the color key
         self.original_image.set_colorkey((255, 255, 255))
 
         # Resize the image to match the platform size
         self.image = self.resize(width, height)
 
-        print(f"Resized image size: {self.image.get_size()}")  # Debug line
-
     def resize(self, width, height):
         # Resize the image to match the platform size
         return pygame.transform.scale(self.original_image, (width, height))
+    
+class EndPlatform(Platform):
+    pass
--- screens/game.py
+++ screens/game.py
@@ -2,6 +2,7 @@
 import pygame
 from components.player import Player
 from components.level import Level
+from components.platform import Platform, EndPlatform
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, WHITE
 from .base import BaseScreen
 from .start import StartScreen
@@ -17,27 +18,33 @@ class GameScreen(BaseScreen):
         self.player = Player()
         self.all_sprites.add(self.player)
 
+        # Initialize camera offset
+        self.camera_offset_x = 0
+
     def process_input(self):
         super().process_input()
         keys = pygame.key.get_pressed()
         self.player.handle_input(keys)
-        
 
     def update(self):
         self.all_sprites.update()
+
+        # Update camera offset
+        self.camera_offset_x = SCREEN_WIDTH // 2 - self.player.rect.x
 
         # Check if the player has fallen off the edge
         if self.player.rect.y > SCREEN_HEIGHT:
             # Pass the GameScreen class to the EndScreen constructor
             self.next_screen = EndScreen(self.__class__)
 
-        # Move the game world in the opposite direction of the player
-        for platform in self.level.get_platforms():
-            platform.rect.x -= self.player.velocity_x
-
         # Check if the player collides with any platforms
         platform_hits = pygame.sprite.spritecollide(self.player, self.level.get_platforms(), False)
-        self.player.handle_platform_collision(platform_hits)
+        for platform in platform_hits:
+            if isinstance(platform, EndPlatform):
+                self.next_screen = EndScreen(self.__class__)
+            else:
+                self.player.handle_platform_collision(platform_hits)
+
 
 
     def render(self, screen):
@@ -46,7 +53,8 @@ class GameScreen(BaseScreen):
 
         # Draw platform images
         for platform in self.level.get_platforms():
-            screen.blit(platform.image, platform.rect.topleft)
+            screen.blit(platform.image, (platform.rect.x + self.camera_offset_x, platform.rect.y))
 
         # Draw player sprite
-        self.all_sprites.draw(screen)
+        screen.blit(self.player.image, (SCREEN_WIDTH // 2, self.player.rect.y))
+
